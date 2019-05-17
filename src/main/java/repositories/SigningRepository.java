3217868131e08bb1a56e01becfd73c3a25133e19
Raul Navarro Cruz <esthalo17@gmail.com>
2019-05-17 16:31:22 +0200

    domain y repositorios

---
+++ src/main/java/repositories/SigningRepository.java
@@ -0,0 +1,12 @@
+
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Signing;
+
+@Repository
+public interface SigningRepository extends JpaRepository<Signing, Integer> {
+
+}
