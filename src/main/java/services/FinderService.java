3ddd478b84cf1363947c46e599d40cd29ee72aa4
angcallla <angcallla@alum.us.es>
2019-05-20 23:08:59 +0200

    repositorio y servicios finder

--- src/main/java/services/FinderService.java
+++ src/main/java/services/FinderService.java
@@ -3,15 +3,22 @@ package services;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 
 import repositories.FinderRepository;
+import security.Authority;
+import domain.Actor;
 import domain.Finder;
+import domain.Manager;
+import domain.Player;
+import domain.President;
 
 @Service
 @Transactional
@@ -28,6 +35,15 @@ public class FinderService {
 
 	@Autowired
 	private Validator			validator;
+
+	@Autowired
+	private ManagerService		managerService;
+
+	@Autowired
+	private PlayerService		playerService;
+
+	@Autowired
+	private PresidentService	presidentService;
 
 
 	public Finder create() {
@@ -57,31 +73,35 @@ public class FinderService {
 
 	}
 
-	//	public Finder save(final Finder finder) {
-	//		Assert.notNull(finder);
-	//
-	//		if (finder.getId() != 0) {
-	//			final Actor actor = this.actorService.findByPrincipal();
-	//			Assert.notNull(actor);
-	//
-	//			final Authority hW = new Authority();
-	//			hW.setAuthority(Authority.ROOKIE);
-	//			if (actor.getUserAccount().getAuthorities().contains(hW)) {
-	//				final Rookie owner = finder.getRookie();
-	//				Assert.notNull(owner);
-	//				Assert.isTrue(actor.getId() == owner.getId());
-	//				finder.setLastUpdate(new Date(System.currentTimeMillis() - 1000));
-	//				final Collection<Position> positionSearchedList = this.positionService.findPositionsByFinder(finder);
-	//				finder.setPositions(positionSearchedList);
-	//			}
-	//		} else
-	//			finder.setLastUpdate(new Date(System.currentTimeMillis() - 1000));
-	//
-	//		final Finder result = this.finderRepository.save(finder);
-	//
-	//		return result;
-	//
-	//	}
+	public Finder save(final Finder finder) {
+		Assert.notNull(finder);
+
+		if (finder.getId() != 0) {
+			final Actor actor = this.actorService.findByPrincipal();
+			Assert.notNull(actor);
+
+			final Authority hW = new Authority();
+			hW.setAuthority(Authority.PRESIDENT);
+			if (actor.getUserAccount().getAuthorities().contains(hW)) {
+				final President owner = finder.getPresident();
+				Assert.notNull(owner);
+				Assert.isTrue(actor.getId() == owner.getId());
+				finder.setLastUpdate(new Date(System.currentTimeMillis() - 1000));
+				Collection<Manager> managerSearchedList = new HashSet<Manager>();
+				if (finder.getPosition() == null || finder.getPosition() == "")
+					managerSearchedList = this.managerService.findManagersByFinder(finder);
+				finder.setManagers(managerSearchedList);
+				final Collection<Player> playerSearchedList = this.playerService.findPlayersByFinder(finder);
+				finder.setPlayers(playerSearchedList);
+			}
+		} else
+			finder.setLastUpdate(new Date(System.currentTimeMillis() - 1000));
+
+		final Finder result = this.finderRepository.save(finder);
+
+		return result;
+
+	}
 
 	public Finder saveAdmin(final Finder finder) {
 
@@ -105,24 +125,24 @@ public class FinderService {
 
 	// Other business rules
 
-	//	public Finder reconstruct(final Finder finder, final BindingResult binding) {
-	//
-	//		final Finder finderBBDD = this.findOne(finder.getId());
-	//
-	//		finder.setRookie(finderBBDD.getRookie());
-	//
-	//		finder.setLastUpdate(new Date(System.currentTimeMillis() - 1000));
-	//
-	//		this.validator.validate(finder, binding);
-	//
-	//		return finder;
-	//
-	//	}
-	//
-	//	public Finder findFinderByRookie(final int rookieId) {
-	//		final Finder finder = this.finderRepository.findFinderByRookie(rookieId);
-	//		return finder;
-	//	}
+	public Finder reconstruct(final Finder finder, final BindingResult binding) {
+
+		final Finder finderBBDD = this.findOne(finder.getId());
+
+		finder.setPresident(finderBBDD.getPresident());
+
+		finder.setLastUpdate(new Date(System.currentTimeMillis() - 1000));
+
+		this.validator.validate(finder, binding);
+
+		return finder;
+
+	}
+
+	public Finder findFinderByPresident(final int presidentId) {
+		final Finder finder = this.finderRepository.findFinderByPresident(presidentId);
+		return finder;
+	}
 	//
 	//	public Collection<Finder> findFindersByPositionId(final int positionId) {
 	//
@@ -131,35 +151,20 @@ public class FinderService {
 	//		return result;
 	//	}
 
-	//	public Boolean security(final int finderId) {
-	//		Boolean res = false;
-	//		Assert.notNull(finderId);
-	//
-	//		final Rookie hackerNow = this.rookieService.findByPrincipal();
-	//
-	//		final Finder finder = this.findOne(finderId);
-	//
-	//		final Rookie owner = finder.getRookie();
-	//
-	//		if (hackerNow.getId() == owner.getId())
-	//			res = true;
-	//
-	//		return res;
-	//	}
+	public Boolean security(final int finderId) {
+		Boolean res = false;
+		Assert.notNull(finderId);
 
-	//	public Finder clear(final Finder finderSearched) {
-	//		Assert.isTrue(this.security(finderSearched.getId()));
-	//
-	//		Finder finder = finderSearched;
-	//
-	//		final Collection<Position> positionsEmpty = new HashSet<Position>();
-	//
-	//		finder.setPositions(positionsEmpty);
-	//
-	//		finder = this.finderRepository.save(finder);
-	//
-	//		return finder;
-	//
-	//	}
+		final President presidentNow = this.presidentService.findByPrincipal();
+
+		final Finder finder = this.findOne(finderId);
+
+		final President owner = finder.getPresident();
+
+		if (presidentNow.getId() == owner.getId())
+			res = true;
+
+		return res;
+	}
 
 }
