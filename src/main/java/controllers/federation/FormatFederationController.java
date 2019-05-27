413e3ba76e6a2a06d5c0e71a8b484b2f803238cc
Agustín Núñez <seviagus@gmail.com>
2019-05-28 01:23:59 +0200

    Format entero

--- src/main/java/controllers/federation/FormatFederationController.java
+++ src/main/java/controllers/federation/FormatFederationController.java
@@ -1,16 +1,27 @@
 package controllers.federation;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import domain.Actor;
+import domain.Competition;
 import domain.Federation;
 import domain.Format;
+import domain.Game;
+import domain.Minutes;
+import domain.Team;
+import services.ActorService;
+import services.CompetitionService;
 import services.ConfigurationService;
 import services.FederationService;
 import services.FormatService;
@@ -30,6 +41,11 @@ public class FormatFederationController {
 	@Autowired
 	private ConfigurationService	configurationService;
 	
+	@Autowired
+	private ActorService			actorService;
+	
+	@Autowired
+	private CompetitionService		competitionService;
 	
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list() {
@@ -46,7 +62,7 @@ public class FormatFederationController {
 
 		result = new ModelAndView("format/list");
 		result.addObject("formats", formats);
-		result.addObject("requestURI", "format/fedearation/list.do");
+		result.addObject("requestURI", "/format/federation/list.do");
 		result.addObject("pagesize", 5);
 		result.addObject("banner", banner);
 		result.addObject("language", LocaleContextHolder.getLocale().getLanguage());
@@ -54,6 +70,113 @@ public class FormatFederationController {
 
 		return result;
 
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		final String banner = this.configurationService.findConfiguration().getBanner();
+
+		final Actor actor = this.actorService.findByPrincipal();
+		
+		Format format = this.formatService.create();
+
+		result = this.createEditModelAndView(format, null);
+		result.addObject("banner", banner);
+		
+		return result;
+
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int formatId) {
+		ModelAndView result;
+
+		final Format format = this.formatService.findOne(formatId);
+
+		final Actor actor = this.actorService.findByPrincipal();
+
+		final String banner = this.configurationService.findConfiguration().getBanner();
+
+		if (format == null || (format.getId() != 0 && format.getFederation().getId() != actor.getId())) {
+			result = new ModelAndView("misc/notExist");
+			result.addObject("banner", banner);
+		} else
+			result = this.createEditModelAndView(format, null);
+		return result;
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@ModelAttribute(value = "format") Format format, final BindingResult binding) {
+		ModelAndView result;
+		final String banner = this.configurationService.findConfiguration().getBanner();
+
+		if (format.getId() != 0 && this.formatService.findOne(format.getId()) == null) {
+			result = new ModelAndView("misc/notExist");
+			result.addObject("banner", banner);
+		} else {
+			format = this.formatService.reconstruct(format, binding);
+			if (binding.hasErrors())
+				result = this.createEditModelAndView(format, null);
+			else
+				try {
+					this.formatService.save(format);
+					result = new ModelAndView("redirect:/format/federation/list.do");
+				} catch (final Throwable oops) {
+					result = this.createEditModelAndView(format, "commit.error");
+
+				}
+		}
+
+		return result;
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(Format format, final BindingResult binding) {
+		ModelAndView result;
+		final String banner = this.configurationService.findConfiguration().getBanner();
+		final Actor actor = this.actorService.findByPrincipal();
+		int id = format.getId();
+		format = this.formatService.findOne(id);
+		
+		Collection<Competition> c = this.competitionService.findByFormatId(format.getId());
+
+		if (format == null || (format.getId() != 0 && format.getFederation().getId() != actor.getId()) || !c.isEmpty()) {
+			result = new ModelAndView("misc/notExist");
+			result.addObject("banner", banner);
+		} else
+			try {
+				this.formatService.delete(format);
+				result = new ModelAndView("redirect:/format/federation/list.do");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(format, "game.commit.error");
+
+			}
+		return result;
+	}
+	
+	protected ModelAndView createEditModelAndView(final Format format, final String messageCode) {
+		final ModelAndView result;
+		
+		int id = format.getId();
+		
+		Collection<Competition> c = this.competitionService.findByFormatId(id);
+		
+		int canDelete = 0;
+		
+		if (c.isEmpty()) {
+			canDelete = 1;
+		}
+
+		final String banner = this.configurationService.findConfiguration().getBanner();
+
+		result = new ModelAndView("format/edit");
+		result.addObject("format", format);
+		result.addObject("banner", banner);
+		result.addObject("canDelete",canDelete);
+		result.addObject("messageError", messageCode);
+
+		return result;
 	}
 
 }
